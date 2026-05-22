7c93f2e09d3919d6d71d42d8eb904b83d37d424e
Kusakabe Youichi <void@merope.pleiades.or.jp>
2026-05-22 16:27:54 +0900

    ビルド番号と更新日時を更新

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -5,7 +5,7 @@ import UIKit
 
 struct ContentView: View {
     private static let sharedDefaults = UserDefaults(suiteName: SettingsKeys.appGroupID)
-    private static let editionUpdatedAtRaw: String = "20260522145355"
+    private static let editionUpdatedAtRaw: String = "20260522162145"
 
     private static func editionDateText(from rawValue: String?) -> String? {
         guard let rawValue,
