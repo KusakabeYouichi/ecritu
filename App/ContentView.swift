dd79a7a1e34edaab300eac7e627d60574a4433b5
Kusakabe Youichi <void@merope.pleiades.or.jp>
2026-06-09 09:21:56 +0900

    ビルド番号を1668へ更新

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -7,7 +7,7 @@ import UIKit
 
 struct ContentView: View {
     private static let sharedDefaults = UserDefaults(suiteName: SettingsKeys.appGroupID)
-    private static let editionUpdatedAtRaw: String = "20260608174653"
+    private static let editionUpdatedAtRaw: String = "20260609091845"
     private static let diagnosticsTimestampFormatter: ISO8601DateFormatter = {
         let formatter = ISO8601DateFormatter()
         formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
